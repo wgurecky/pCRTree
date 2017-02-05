d03082ba6b60b71705924e00587c16eb9f48b687
hephaestus <william.gurecky@gmail.com>
2017-02-04 19:01:47 -0600

    update name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'pdRTee',
+    name = 'pCRTree',
     packages = find_packages(),
     author = 'William Gurecky',
     author_email = 'william.gurecky@utexas.edu',
