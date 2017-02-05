18c9dc0b8c99e4bb9bc71dd7eb4168e1782957f1
hephaestus <william.gurecky@gmail.com>
2017-02-04 19:32:06 -0600

    adding license file

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages = find_packages(),
     author = 'William Gurecky',
     author_email = 'william.gurecky@utexas.edu',
-    license = 'MIT',
+    license = 'BSD-3',
     install_requires=['numpy>=1.8.0', 'scipy'],
 )
