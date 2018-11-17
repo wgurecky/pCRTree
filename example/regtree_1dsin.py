95d90715fd7c74f14ebb70c04700409cc69e2d52
William Gurecky <william.gurecky@gmail.com>
2018-11-16 21:33:08 -0600

    updates 1d sin fig

--- example/regtree_1dsin.py
+++ example/regtree_1dsin.py
@@ -7,7 +7,8 @@ from dtree.regress import RegTree
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+sns.set_palette(sns.diverging_palette(250, 15, s=70, l=45, n=3, center="light"))
+sns.set_color_codes()
 
 def main():
     # run simple 1d regression tree example
@@ -18,19 +19,26 @@ def main():
     y = y + yNoise
     regressionTree = RegTree(x, y, maxDepth=4)
     regressionTree.fitTree()
-    regressionTree3 = RegTree(x, y, maxDepth=3)
-    regressionTree3.fitTree()
+    regressionTree2 = RegTree(x, y, maxDepth=2)
+    regressionTree2.fitTree()
+    regressionTree1 = RegTree(x, y, maxDepth=1)
+    regressionTree1.fitTree()
 
     # predict
     xTest = np.linspace(0, 2 * np.pi, n * 2)
     yhat = regressionTree.predict(xTest)
-    yhat3 = regressionTree3.predict(xTest)
+    yhat2 = regressionTree2.predict(xTest)
+    yhat1 = regressionTree1.predict(xTest)
 
     # plot
     plt.figure()
-    plt.plot(x, y, label="Train Data")
-    plt.plot(xTest, yhat, label="Tree Depth=4")
-    plt.plot(xTest, yhat3, label="Tree Depth=3")
+    plt.scatter(x, y, label="Train Data", s=4, c='k')
+    plt.plot(xTest, yhat, label="Tree Depth=4", c='k')
+    plt.plot(xTest, yhat2, label="Tree Depth=2", c='r', ls='--')
+    plt.plot(xTest, yhat1, label="Tree Depth=1", c='b', ls='-.')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(alpha=0.5, ls='--')
     plt.legend()
     plt.savefig('1d_regression_ex.png')
 
